43dfa3c25b090a485412ab2347cad53a4300d12a
tamer_maarouf <tamermaarouf@gmail.com>
2024-11-25 08:53:58 +0200

    4. Buy buy buy

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,8 +1,12 @@
 -- Write a SQL script that creates a trigger that decreases the quantity of an item after adding a new order.
 
+DELIMITER $$
 CREATE TRIGGER dec_quantity
 AFTER
 INSERT ON orders
 FOR EACH ROW BEGIN UPDATE items
 	SET items.quantity = items.quantity-NEW.number
 	WHERE items.name = NEW.item_name;
+
+END$$
+DELIMITER;
