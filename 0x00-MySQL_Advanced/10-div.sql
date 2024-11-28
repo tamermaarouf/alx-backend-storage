94ff53042428eefc6cc95a3434e984a6e5b5cfb8
tamer_maarouf <tamermaarouf@gmail.com>
2024-11-28 23:16:26 +0200

    11. No table for a meeting

--- 0x00-MySQL_Advanced/10-div.sql
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -5,13 +5,13 @@ DELIMITER $$
 DROP FUNCTION IF EXISTS SafeDiv;
 CREATE FUNCTION SafeDiv(a INT, b INT)
 RETURNS FLOAT DETERMINISTIC
-BEGIN 
-DECLARE result FLOAT;
-SET result = 0;
-IF b = 0 THEN
-return result;
-ELSE
-SET result = a / b;
+BEGIN
+	DECLARE result FLOAT;
+	SET result = 0;
+	IF b = 0 THEN
+		return result;
+	ELSE
+		SET result = a / b;
 END IF;
 return result;
 END $$
