d84696b436ffc3ff6ec3011fac220f0d4489df89
tamer_maarouf <tamermaarouf@gmail.com>
2024-11-28 11:34:18 +0200

    6. Add bonus

--- 0x00-MySQL_Advanced/6-bonus.sql
+++ 0x00-MySQL_Advanced/6-bonus.sql
@@ -1,15 +1,24 @@
+-- Write a SQL script that creates a stored procedure AddBonus that adds a new correction for a student.
+-- Requirements:
+-- Procedure AddBonus is taking 3 inputs (in this order):
+-- user_id, a users.id value (you can assume user_id is linked to an existing users)
+-- project_name, a new or already exists projects - if no projects.name found in the table, you should create it
+-- score, the score value for the correction
+
+
 DELIMITER $$
 
-CREATE PROCEDURE IF NOT EXISTS AddBonus (IN userID INT, IN project_name VARCHAR(255), IN scoreID INT)
+DROP PROCEDURE IF EXISTS AddBonus $$
+CREATE PROCEDURE AddBonus(IN userID INT, IN project_name VARCHAR(255), IN scoreID INT)
 BEGIN
-    DECLARE project_id INT;
-    -- SET @project_id = (SELECT '1' FROM projects WHERE projects.name=project_name);
-    IF EXISTS (SELECT '#' FROM projects WHERE projects.name=project_name INTO projectID)
-        BEGIN
-            INSERT INTO corrections (user_id, project_id, score) VALUES (userID, projectID, scoreID);
-        END
-    ELSE
-        SELECT 'NOT FOUND';
-    END IF;
-END$$
+	DECLARE projectID INT;
+	SELECT projects.id INTO projectID FROM projects WHERE projects.name=project_name;
+	IF projectID > 0 THEN
+		INSERT INTO corrections(user_id, project_id, score) VALUES (userID , projectID , scoreID );
+	ELSE
+		INSERT INTO projects (name) VALUES (project_name);
+		SET @project_py = LAST_INSERT_ID();
+		INSERT INTO corrections(user_id, project_id, score) VALUES (userID, @project_py,scoreID);
+END IF;
+END $$
 DELIMITER ;
