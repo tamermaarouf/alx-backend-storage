5be9e9a2f6379ce377c8d6dcf139a6da95d62216
tamer_maarouf <tamermaarouf@gmail.com>
2024-12-04 03:11:40 +0200

    11. Where can I learn Python?

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -6,5 +6,5 @@ Write a Python function that returns the list of school having a specific topic:
 
 def schools_by_topic(mongo_collection, topic):
     ''' return list of school'''
-    topic = mongo_collection.find_one({"topics":topic})
+    topic = mongo_collection.find({"topics":topic})
     return topic
