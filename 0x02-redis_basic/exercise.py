ecde824a6388c93584948f1450908f9f0727eda2
tamer_maarouf <tamermaarouf@gmail.com>
2024-12-07 18:11:52 +0200

    0. Writing strings to Redis

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''Create a Cache class'''
+import redis
+import uuid
+
+
+class Cache():
+    '''method, store an instance of the Redis
+    client as a private variable named _redis
+    '''
+    def __init__(self):
+        self._redis = redis.Redis()
+
+
+    def store(self, data):
+        create_key = str(uuid.uuid4())
+        self._redis.set(create_key, data)
+        return (create_key)
