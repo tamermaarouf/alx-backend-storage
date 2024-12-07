df49645fc8a055a0fd4e354cca240076e80e8588
tamer_maarouf <tamermaarouf@gmail.com>
2024-12-07 18:15:25 +0200

    0. Writing strings to Redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -11,7 +11,6 @@ class Cache():
     def __init__(self):
         self._redis = redis.Redis()
 
-
     def store(self, data):
         create_key = str(uuid.uuid4())
         self._redis.set(create_key, data)
